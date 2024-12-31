78e00f9ad7853026e16ec454106fed96633aebb8
SecretActionDeveloper <secretactiondevdata@gmail.com>
2024-12-30 20:19:17 -0500

    Create analysis_part1.py

---
+++ t0_analysis/analysis_part1.py
@@ -0,0 +1,21 @@
+import os
+import ast
+import pandas as pd
+
+directory = "time_t0_data\\time_t0_processed"
+
+all_features = []
+for root, dirs, files in os.walk(directory):
+    for file_name in files:
+        file_path = os.path.join(directory, file_name)
+        df = pd.read_csv(file_path)
+        for i in range(len(df.index)):
+            features_raw = df["Processed_Features"][i]
+            features = ast.literal_eval(features_raw)
+            all_features.extend(features)
+
+print("Number of features: "  +str(len(all_features)))
+print("Number of unique features " + str(len(list(set(all_features)))))
+
+# Number of features: 27,434
+# Number of unique features 13,994
